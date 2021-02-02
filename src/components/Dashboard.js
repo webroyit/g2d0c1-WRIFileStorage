094e1739524d7450b043794b4cd4acdf572755d7
royweb <xytank@gmail.com>
2021-02-02 14:15:28 -0500

    Add dashboard page

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Alert, Card, Button } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+
+import { useAuth } from '../contexts/AuthContext'
 
 function Dashboard() {
+    const [error, setError] = useState('')
+
+    const { currentUser } = useAuth()
+
     return (
         <div>
-            Dashboard
+            <Card className="mt-4">
+                <Card.Body>
+                    <h2 className="text-center">Profile</h2>
+                    {error && <Alert variant="danger">{error}</Alert>}
+
+                    <strong>Email: </strong> {currentUser.email}
+                    <Link className="btn btn-primary w-100 mt-3" to="/update-profile">
+                        Update Profile
+                    </Link>
+                </Card.Body>
+            </Card>
+            <div className="w-100 text-center mt-2">
+                <Button variant="link">
+                    Log Out
+                </Button>
+            </div>
         </div>
     )
 }
