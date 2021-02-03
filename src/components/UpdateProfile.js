b5bc8754dbd798e7d50ce60e0582287c056c3ff3
royweb <xytank@gmail.com>
2021-02-03 13:54:31 -0500

    Use Promise.all() to handle updateEmail() and updatePassword()

--- src/components/UpdateProfile.js
+++ src/components/UpdateProfile.js
@@ -10,12 +10,36 @@ function UpdateProfile() {
     const passwordRef = useRef()
     const passwordConfirmRef = useRef()
 
-    const { currentUser } = useAuth()
+    const { currentUser, updateEmail, updatePassword } = useAuth()
     const [error, setError] = useState('')
     const [loading, setLoading] = useState(false)
 
-    async function handleSubmit(e) {
+    function handleSubmit(e) {
         e.preventDefault()
+
+        if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+            return setError('Both passwords need to match')
+        }
+
+        const promises = []
+        setLoading(true)
+        setError('')
+
+        if (emailRef.current.value !== currentUser.email) {
+            promises.push(updateEmail(emailRef.current.value))
+        }
+
+        if (passwordRef.current.value) {
+            promises.push(updatePassword(passwordRef.current.value))
+        }
+
+        Promise.all(promises).then(() => {
+            history.push('/')
+        }).catch(() => {
+            setError('Something went wrong on updating your account')
+        }).finally(() => {
+            setLoading(false)
+        })
     }
 
     return (
@@ -32,11 +56,11 @@ function UpdateProfile() {
                         </Form.Group>
                         <Form.Group id="password">
                             <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" ref={passwordRef} required placeholder="Leave blank to keep the same" />
+                            <Form.Control type="password" ref={passwordRef} placeholder="Leave blank to keep the same" />
                         </Form.Group>
                         <Form.Group id="password-confirm">
                             <Form.Label>Password Confirmation</Form.Label>
-                            <Form.Control type="password" ref={passwordConfirmRef} required placeholder="Leave blank to keep the same" />
+                            <Form.Control type="password" ref={passwordConfirmRef} placeholder="Leave blank to keep the same" />
                         </Form.Group>
                         <Button className="w-100" type="submit" disabled={loading}>Update</Button>
                     </Form>
