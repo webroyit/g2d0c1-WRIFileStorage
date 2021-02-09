f9e171352deeae7ec8091920e9412d0bf5bc6032
royweb <xytank@gmail.com>
2021-02-09 13:25:01 -0500

    Add modal for adding folder

--- src/components/google-drive/AddFolderButton.js
+++ src/components/google-drive/AddFolderButton.js
@@ -1,16 +1,48 @@
-import React from 'react'
-import { Button } from 'react-bootstrap'
+import React, { useState } from 'react'
+import { Modal, Form, Button } from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFolderPlus } from '@fortawesome/free-solid-svg-icons'
 
 function AddFolderButton() {
-    function openModal(){
+    const [open, setOpen] = useState(false);
+    const [name, setName] = useState("");
 
+    function openModal(){
+        setOpen(true)
+    }
+
+    function closeModal(){
+        setOpen(false)
     }
     return (
-        <Button onClick={openModal} variant="outline-success" size="sm">
-            <FontAwesomeIcon icon={faFolderPlus} />
-        </Button>
+        <>
+            <Button onClick={openModal} variant="outline-success" size="sm">
+                <FontAwesomeIcon icon={faFolderPlus} />
+            </Button>
+            <Modal show={open} onHide={closeModal}>
+                <Form>
+                    <Modal.Body>
+                        <Form.Group>
+                            <Form.Label>Folder Name</Form.Label>
+                            <Form.Control
+                                type="text"
+                                required
+                                value={name}
+                                onChange={e => setName(e.target.value)}
+                            />
+                        </Form.Group>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={closeModal}>
+                            Close
+                        </Button>
+                        <Button variant="success" type="submit">
+                            Add Folder
+                        </Button>
+                    </Modal.Footer>
+                </Form>
+            </Modal>
+        </>
     )
 }
 
