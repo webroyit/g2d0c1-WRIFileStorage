45500549bf7809599119fad3900cc8556efe202a
royweb <xytank@gmail.com>
2021-02-09 14:57:24 -0500

    Add a function to create folder data on firebase

--- src/components/google-drive/AddFolderButton.js
+++ src/components/google-drive/AddFolderButton.js
@@ -3,9 +3,13 @@ import { Modal, Form, Button } from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFolderPlus } from '@fortawesome/free-solid-svg-icons'
 
+import { database } from '../../firebase'
+import { useAuth } from '../../contexts/AuthContext'
+
 function AddFolderButton() {
     const [open, setOpen] = useState(false);
     const [name, setName] = useState("");
+    const { currentUser } = useAuth()
 
     function openModal(){
         setOpen(true)
@@ -14,13 +18,27 @@ function AddFolderButton() {
     function closeModal(){
         setOpen(false)
     }
+
+    function handleSubmit(e){
+        e.preventDefault()
+
+        // Create a folder in the database
+        database.folders.add({
+            name: name,
+            userId: currentUser.uid
+        })
+
+        setName("")
+        closeModal()
+    }
+
     return (
         <>
             <Button onClick={openModal} variant="outline-success" size="sm">
                 <FontAwesomeIcon icon={faFolderPlus} />
             </Button>
             <Modal show={open} onHide={closeModal}>
-                <Form>
+                <Form onSubmit={handleSubmit}>
                     <Modal.Body>
                         <Form.Group>
                             <Form.Label>Folder Name</Form.Label>
