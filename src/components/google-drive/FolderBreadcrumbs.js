84c45daf03bbe1857b80d77f038c2cd804459fe5
royweb <xytank@gmail.com>
2021-02-14 14:19:45 -0500

    Display the rest of the links in the breadcrumb

--- src/components/google-drive/FolderBreadcrumbs.js
+++ src/components/google-drive/FolderBreadcrumbs.js
@@ -1,9 +1,30 @@
 import React from 'react'
 import { Breadcrumb } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+
+import { ROOT_FOLDER } from '../hooks/useFolder'
 
 function FolderBreadcrumbs({ currentFolder }) {
+    // Not save the root folder
+    let path = currentFolder === ROOT_FOLDER ? [] : [ROOT_FOLDER]
+
+    if (currentFolder) path = [...path, ...currentFolder.path]    
+
     return (
         <Breadcrumb className="flex-grow-1" listProps={{ className: "bg-white pl-0 m-0"}}>
+            {path.map((folder, index) => (
+                <Breadcrumb.Item
+                    key={folder.id}
+                    linkAs={Link}
+                    linkProps={{
+                        to: folder.id ? `/folder/${folder.id}` : '/'
+                    }}
+                    className="text-truncate d-inline-block"
+                    style={{ maxWidth: "150px"}}
+                >
+                    {folder.name}
+                </Breadcrumb.Item>
+            ))}
             {currentFolder && (
                 <Breadcrumb.Item
                     className="text-truncate d-inline-block"
